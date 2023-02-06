4106aed486fd44835092e78afc2356c0b03950a8
Shrutika Troup <shrutika.gupta@rook.capital>
2023-02-06 19:25:48 +0000

    github block

--- week_2_workflow_orchestration/flows/02_gcp/etl_web_to_gcs_shru.py
+++ week_2_workflow_orchestration/flows/02_gcp/etl_web_to_gcs_shru.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import pandas as pd
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
+from prefect.filesystems import GitHub
 
 
 @task(log_prints=True)
@@ -32,18 +33,24 @@ def write_gcs(path: Path) -> None:
     gcs_block.upload_from_path(from_path=path, to_path=path)
     return
 
+@task(log_prints=True)
+def write_github(path: Path) -> None:
+    github_block = GitHub.load("de-zoom")
+    github_block.upload_from_path(from_path=path, to_path=path)
+    return
+
 @flow(log_prints=True)
 def etl_web_to_gcs() -> None:
     """This is the main ETL flow """
     color = 'green'
     year = '2020'
-    month = 1
+    month = 11
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz" 
     df = fetch(dataset_url)
     df_clean = clean(df)
     path = write_local(df_clean, color, dataset_file)
-    write_gcs(path)
+    write_github(path)
 
 if __name__ == '__main__':
     etl_web_to_gcs()
